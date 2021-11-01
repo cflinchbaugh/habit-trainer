22fe6c1c781c0b90dccc5d9ff71f48bd14fbc4ef
Christopher Flinchbaugh <chris.f@Very-MacBook-Pro.local>
2021-11-01 15:15:02 -0400

    Render recycler view with dummy data

---
+++ app/src/main/java/com/example/habittrainer/HabitsAdapter.kt
@@ -0,0 +1,42 @@
+package com.example.habittrainer
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class HabitsAdapter(private val habits: List<Habit>) : RecyclerView.Adapter<HabitsAdapter.HabitViewHolder>() {
+
+    class HabitViewHolder(private val card: View) : RecyclerView.ViewHolder(card) {
+        private val tvTitle: TextView = card.findViewById(R.id.tv_title)
+        private val tvDescription: TextView = card.findViewById(R.id.tv_description)
+        private val tvIcon: ImageView = card.findViewById(R.id.tv_icon)
+
+        fun bind(habit: Habit) {
+            tvTitle.text = habit.title
+            tvDescription.text = habit.description
+            tvIcon.setImageResource(habit.image)
+        }
+    }
+
+    //  Create a new ViewHolder for a single card
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): HabitViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.single_card, parent, false)
+
+        return HabitViewHolder(view)
+    }
+
+    // Fills in the dynamic data into a reused card (ex: Card 1 scrolls off screen, Card 4 scrolls into view- reuse the Card 1 but update the content with Card 4 data)
+    override fun onBindViewHolder(holder: HabitViewHolder, position: Int) {
+        holder.bind(habits[position])
+    }
+
+    override fun getItemCount(): Int {
+        return habits.size
+    }
+
+
+}
--- app/src/main/java/com/example/habittrainer/MainActivity.kt
+++ app/src/main/java/com/example/habittrainer/MainActivity.kt
@@ -4,36 +4,23 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+import android.content.Context
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var tvDescription: TextView
-    private lateinit var tvIcon: ImageView
-    private lateinit var tvTitle: TextView
-
-    private fun initIcon() {
-        tvIcon = findViewById(R.id.tv_icon)
-        tvIcon.setImageResource(R.drawable.water)
-        tvIcon.contentDescription = getString(R.string.iconWater)
-    }
-
-    private fun initTitle() {
-        tvTitle = findViewById(R.id.tv_title)
-        tvTitle.text = getString(R.string.drinkWater)
-    }
-
-    private fun initDescription() {
-        tvDescription = findViewById(R.id.tv_description)
-        tvDescription.text = getString(R.string.stayHydrated)
-    }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        initIcon()
-        initTitle()
-        initDescription()
+        // Adapter -> defines data
+        val recyclerView: RecyclerView = findViewById(R.id.recycler_view)
+        val habitData = Habit("", "", 0)
+        val sampleList = habitData.getSampleHabits()
+        recyclerView.adapter = HabitsAdapter(sampleList)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.setHasFixedSize(true)
+
     }
-
-
 }
